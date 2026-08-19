336a06588836899fcd384d1c8d1dfc5a2ff72d01
DBegoRossini <debora.rossini@grupoimpper.com.br>
2026-08-19 17:40:25 -0300

    Use environment variables for app configuration
    Replaced hardcoded configuration values with environment variables for better security and flexibility.

--- api/app_config.py
+++ api/app_config.py
@@ -3,12 +3,15 @@ import os
 
 load_dotenv()
 
-AUTHORITY =  {{impperFlows.AUTHORITY}}
-CLIENT_ID = {{impperFlows.CLIENT_ID}}
-CLIENT_SECRET = {{impperFlows.CLIENT_SECRET}}
-REDIRECT_URI = {{impperFlows.REDIRECT_URI}}
-SENHA_BANCO = {{impperFlows.senha_banco}}
-US_BANCO = {{impperFlows.us_banco}}
+AUTHORITY = os.getenv("AUTHORITY")
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+REDIRECT_URI = os.getenv("REDIRECT_URI")
+SENHA_BANCO = os.getenv("senha_banco")
+US_BANCO = os.getenv("us_banco")
+SSH_PASSPHRASE = os.getenv("SSH_PASSPHRASE")
+SS_PATH = os.getenv("ss_path")
+SS_USER = os.getenv("ss_user")
 SESSION_TYPE = os.getenv("SESSION_TYPE", "filesystem")
 SESSION_FILE_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..", "flask_session")
 SESSION_PERMANENT = False
