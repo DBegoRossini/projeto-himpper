2530dc63254e1394756c21a000f789fa00ffcb18
DBegoRossini <debora.rossini@grupoimpper.com.br>
2026-08-20 17:01:51 -0300

    Refactor database connection settings
    Updated database connection configuration to use default port and allow fallback for user and password environment variables.

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -23,15 +23,15 @@ try:
 except Exception as e:
     print("REDIS ERROR:", repr(e))
 
-db_host = os.getenv("DB_HOST")
-db_port = os.getenv("DB_PORT")
+db_host = os.getenv("DB_HOST")         
+db_port = os.getenv("DB_PORT", "5432")
 db_name = os.getenv("DB_NAME")
-db_user = os.getenv("us_banco")
-db_pass = os.getenv("senha_banco")
+db_user = os.getenv("us_banco") or os.getenv("DB_USER")
+db_pass = os.getenv("senha_banco") or os.getenv("DB_PASSWORD")
 
-db_url = f"postgresql://{quote_plus(db_user)}:{quote_plus(db_pass)}@{db_host}:{db_port}/{db_name}"
-
-app.config["SQLALCHEMY_DATABASE_URI"] = db_url
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"postgresql://{quote_plus(db_user)}:{quote_plus(db_pass)}@{db_host}:{db_port}/{db_name}"
+)
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 database = SQLAlchemy(app)
