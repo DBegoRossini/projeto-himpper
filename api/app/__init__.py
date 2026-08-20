5d97cf5ed8815eca33dbbb2e4d2543be2c47f925
DBegoRossini <debora.rossini@grupoimpper.com.br>
2026-08-20 08:41:32 -0300

    Add ProxyFix middleware to WSGI app

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -4,6 +4,7 @@ from identity.flask import Auth
 import os
 import app_config
 from urllib.parse import quote_plus
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
